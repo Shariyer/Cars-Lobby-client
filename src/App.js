3ed1ce87292108fb7e954d247b203bbe4d5d84c9
shariyer <s.m.shariyer04@gmail.com>
2022-11-24 00:41:21 +0600

    tailwind css, daisyui,react-hot-toast,react-router-dom,react-icons installation and simple folder structure added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 /** @format */
-
-import logo from "./logo.svg";
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import router from "./Router/Router";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+      <Toaster></Toaster>
+    </div>
+  );
 }
 
 export default App;
